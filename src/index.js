4911264176f9a835b913a64f148c734a908fddad
Victoria Duggan <victoria.duggan@shopify.com>
2019-01-18 10:08:52 -0800

    setup redux, event handlers, boiler plate for cart

--- src/index.js
+++ src/index.js
@@ -4,13 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Client from 'shopify-buy';
+import { Provider } from 'react-redux';
+import store from './store';
+import './styles/shopify.css';
 
+// build shopify client
 const client = Client.buildClient({
-  storefrontAccessToken: 'dfe36e96a80046885f8167d102f063e9',
-  domain: 'api-test-store-vix.myshopify.com'
+    storefrontAccessToken: 'dfe36e96a80046885f8167d102f063e9',
+    domain: 'api-test-store-vix.myshopify.com'
+});
+store.dispatch({type: 'CLIENT_CREATED', payload: client});
+
+// buildClient() is synchronous, so we can call all these after!
+client.product.fetchAll().then((res) => {
+  store.dispatch({type: 'PRODUCTS_FOUND', payload: res});
+});
+client.checkout.create().then((res) => {
+  store.dispatch({type: 'CHECKOUT_FOUND', payload: res});
+});
+client.shop.fetchInfo().then((res) => {
+  store.dispatch({type: 'SHOP_FOUND', payload: res});
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,  
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
