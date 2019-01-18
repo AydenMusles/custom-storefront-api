03b91f4cd4fab15b5d852a8703bad39b989510a2
Victoria Duggan <victoria.duggan@shopify.com>
2019-01-18 08:56:50 -0800

    add and conigure shopify-buy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Client from 'shopify-buy';
+
+const client = Client.buildClient({
+  storefrontAccessToken: 'dfe36e96a80046885f8167d102f063e9',
+  domain: 'api-test-store-vix.myshopify.com'
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
